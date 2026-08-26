17e387b504b76a5868b88a2932814396777497bf
Mamoshebe Mokhathi <mamoshebimokhathi@gmail.com>
2026-08-27 01:30:00 +0200

    Configure Spring Boot MySQL connection

---
+++ src/main/java/com/shebi/secure_api/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.shebi.secure_api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Secure API is running";
+
+
+    }
+}
